c5e1e56752d73aa4c1bb7b09a638c251e526ed3c
JoaoVitorLima242 <jlima0500@gmail.com>
2022-05-17 22:33:43 -0300

    create welcome

--- src/components/views/Welcome/index.tsx
+++ src/components/views/Welcome/index.tsx
@@ -4,8 +4,10 @@ const Welcome = () => {
 
     return(
         <S.Wrapper>
-            <div className="content">
-                
+            <div className="content">               
+                <h1>Full-Stack Web Developer</h1>
+
+                <button>Tell me more</button>
             </div>
         </S.Wrapper>
     )
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import { GlobalStyle } from '../styles/globals'
+import { GlobalStyle } from '../styles/global'
 
 function App({ Component, pageProps }: AppProps) {
   return (
