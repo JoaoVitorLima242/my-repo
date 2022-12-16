2db8cea464e17537db875f462ecfa88cdff8ed0c
JoaoVitorLima242 <jlima0500@gmail.com>
2022-12-15 22:17:23 -0300

    adjusting styles

--- src/components/views/About/index.tsx
+++ src/components/views/About/index.tsx
@@ -36,9 +36,9 @@ const About = () => {
                     <h1>About Me</h1>
                     <p>Hi! Nice to meet ...</p>
                 </S.CustomTitle>
-                <div className="content">
+                <S.Content>
                     <div className="d-flex">
-                        <div className="profile-img">
+                        <div>
                             <Image 
                                 src='/img/my_photo.jpeg'
                                 alt="Uma foto minha sorrindo" 
@@ -46,36 +46,35 @@ const About = () => {
                                 height={400}
                             />
                         </div>
-                        <div className="info">
-                        <div className="line">
-                            <div className="ball"></div>
-                        </div>
-                        <div className="text">
-                            <div className="d-flex flex-column">
-                                <h5>João Vitor Brietzke de Lima</h5>
-                                <span><FaHouseUser/> Porto Alegre - Brazil</span>
+                        <S.Info>
+                            <S.Line>
+                                <S.Ball />
+                            </S.Line>
+                            <div>
+                                <div className="d-flex flex-column">
+                                    <h5>João Vitor Brietzke de Lima</h5>
+                                    <span><FaHouseUser/> Porto Alegre - Brazil</span>
+                                </div>
+                                <p>Hello, I’m João Vitor and I’m a software engineer. I chose this profession because is a really great way to develop virtual ideas and bring them to reality.</p>
+                                <p>I have been surfing in this ocean of knowledge and I&lsquo;m growing increasingly passionate about facing the new challenges found in this field.</p>
+                                <p>I consider myself a hardworking and creative person. I have been careful to stay in touch with the latest technologies, using only the latest cutting-edge material, within my work.</p>
                             </div>
-                            <p>Hello, I’m João Vitor and I’m a software engineer. I chose this profession because is a really great way to develop virtual ideas and bring them to reality.</p>
-                            <p>I have been surfing in this ocean of knowledge and I&lsquo;m growing increasingly passionate about facing the new challenges found in this field.</p>
-                            <p>I consider myself a hardworking and creative person. I have been careful to stay in touch with the latest technologies, using only the latest cutting-edge material, within my work.</p>
-                        </div>
-                        </div>
+                        </S.Info>
                     </div>
-                    <div className="icons">
+                    <S.Icons>
                         {cards.map(({title, text, icon}) => (
                             <>
-                                <div className="icon">
+                                <S.Icon>
                                     {icon}
-                                </div>
-                                <div className="text">
+                                </S.Icon>
+                                <div>
                                     <h6>{title}</h6>
                                     <span>{text}</span>
                                 </div>
                             </>
                         ))}
-                    </div>
-                </div>
-
+                    </S.Icons>
+                </S.Content>
             </Container>
         </S.Wrapper>
     )
--- src/components/views/About/styles.ts
+++ src/components/views/About/styles.ts
@@ -3,109 +3,106 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
     background-color: #44475a;
     padding: 40px 20px;
-    
-    .content {
-        display: flex;
-        align-items: flex-start;
-        flex-direction: column;
-        justify-content: space-between;
-        background-color: #282a36;
-        padding: 20px 40px;
-        margin: 20px;
-        border-radius: 12px;
-        position: relative;
+`
 
-        img {
-            border-radius: 50%;
-        }
+export const Content = styled.div`
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    justify-content: space-between;
+    background-color: #282a36;
+    padding: 20px 40px;
+    margin: 20px;
+    border-radius: 12px;
+    position: relative;
 
-        .info {
-            display: flex;
+    img {
+        border-radius: 50%;
+    }
+`
 
-            h5 {
-                font-family: 'Lobster Two',cursive;
-                font-weight: 700;
-                text-shadow: 4px 2px 0  #f8f8f220;
-                color: #ff79c6;
-                font-size: 30px;
-            }
+export const Info = styled.div`
+    display: flex;
 
-            p {
-                font-size: 21px;
-                color: white;
-                margin: 18px 0 ;
-            }
-
-            span {
-                color: #ff79c6;
-            }
-
-            .line {
-                width: 8px;
-                border-radius: 4px;
-                background-color: #ff79c6;
-                height: auto;
-                margin: 0 20px;
-                position: relative;
-                
-                .ball {
-                    position: absolute;
-                    top: 8px;
-                    right: -10px;
-                    width: 16px;
-                    height: 16px;
-                    border: solid 4px #994876;
-                    box-shadow: 0px 0px 19px 0px #994876;
-                    border-radius: 50%;
-                    background-color: #ff79c6;
-                }
-            }
-        }
-
-        .icons {
-            display: flex;
-            align-items: center;
-            justify-content: flex-end;
-            padding: 35px 40px 5px;
-            margin-left: auto;
-            
-            .icon {
-                width: 46px;
-                height: 46px;
-                border: solid 4px #994876;
-                box-shadow: 0px 0px 19px 0px #994876;
-                border-radius: 50%;
-                background-color: #ff79c6;
-                display: flex;
-                align-items: center;
-
-                
-                svg {
-                    width: 30px;
-                    height: 30px;
-                    color: white;
-                    margin: 0 auto;
-                }
-
-            }
-
-            h6, span {
-                margin-left: 10px;
-                margin-right: 60px;
-            }
-
-            h6 {
-                margin-bottom: 0 !important;
-                color: white;
-            }
-            
-            span {
-                color: #ff79c6;
-            } 
-        }
-
+    h5 {
+        font-family: 'Lobster Two',cursive;
+        font-weight: 700;
+        text-shadow: 4px 2px 0  #f8f8f220;
+        color: #ff79c6;
+        font-size: 30px;
     }
 
+    p {
+        font-size: 21px;
+        color: white;
+        margin: 18px 0 ;
+    }
+
+    span {
+        color: #ff79c6;
+    }
+`
+
+export const Line = styled.div`
+    width: 8px;
+    border-radius: 4px;
+    background-color: #ff79c6;
+    height: auto;
+    margin: 0 20px;
+    position: relative;
+`
+
+export const Ball = styled.div`
+    position: absolute;
+    top: 8px;
+    right: -10px;
+    width: 16px;
+    height: 16px;
+    border: solid 4px #994876;
+    box-shadow: 0px 0px 19px 0px #994876;
+    border-radius: 50%;
+    background-color: #ff79c6;
+`
+
+export const Icons = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding: 35px 40px 5px;
+    margin-left: auto;
+
+    h6, span {
+        margin-left: 10px;
+        margin-right: 60px;
+    }
+
+    h6 {
+        margin-bottom: 0 !important;
+        color: white;
+    }
+    
+    span {
+        color: #ff79c6;
+    } 
+`
+
+export const Icon = styled.div`
+    width: 46px;
+    height: 46px;
+    border: solid 4px #994876;
+    box-shadow: 0px 0px 19px 0px #994876;
+    border-radius: 50%;
+    background-color: #ff79c6;
+    display: flex;
+    align-items: center;
+
+    
+    svg {
+        width: 30px;
+        height: 30px;
+        color: white;
+        margin: 0 auto;
+    }
 `
 
 export const CustomTitle = styled.div`
